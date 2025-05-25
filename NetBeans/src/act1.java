736db8c6b8e64d6b9918e8670c7db45d92850c37
alasjeys <jeysi0418@gmail.com>
2025-05-25 10:07:41 +0800

    Add files via upload

---
+++ NetBeans/src/act1.java
@@ -0,0 +1,9 @@
+package netbeans;
+        
+public class act1 {
+    public static void main(String[] args) {
+        System.out.println("John Christian Limpin");
+        System.out.println("24-050363");
+        System.out.println("BS in Computer Engineering");
+    }
+}
---
+++ NetBeans/src/netbeans/NetBeans.java
@@ -0,0 +1,9 @@
+package netbeans;
+
+public class NetBeans {
+
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+    
+}
